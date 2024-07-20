d62b59f985bf8daa93c2219698dafb14b0095d2c
Steven_Villegas <syivenvillegas0@gmail.com>
2024-07-19 22:01:08 -0500

    contador clics

--- src/Components/Pages/ContadorClics/ContadorClics.jsx
+++ src/Components/Pages/ContadorClics/ContadorClics.jsx
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react';
 
 function ContadorClics() {
-  const [contador, setContador] = useState(0);
+  const [contador, setContador] = useState(0); // Inicializa el contador en 0
+
+  useEffect(() => {
+      // Actualiza el título del documento cada vez que cambia el contador
+      document.title = `Contador: ${contador}`;
+  }, [contador]);
 
   const incrementarContador = () => {
-    setContador(contador + 1);
+      setContador(contador + 1);
   };
 
   return (
-    <div>
-      <h2>Contador de Clics</h2>
-      <p>Contador: {contador}</p>
-      <button onClick={incrementarContador}>Incrementar</button>
-    </div>
+      <div className="contador-container">
+          <h1 className="contador-title">Contador: {contador}</h1>
+          <button className="increment-button" onClick={incrementarContador}>Incrementar</button>
+      </div>
   );
 }
 
